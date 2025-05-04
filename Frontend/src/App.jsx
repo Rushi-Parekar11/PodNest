58a8a573e6a751675c580d0538899b43fa755135
Rushi-Parekar11 <rushikeshparekar11@gmail.com>
2025-05-04 23:11:51 +0530

    add first

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+    <div className='h-[100vh] w-[100%] bg-gray-600'>
+      <h1 className='text-red-500'>hi</h1>
+    </div>
+    </>
+
+  )
+}
+
+export default App
